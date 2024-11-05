f257314a532a76eccbf719c7e61ea783162faba5
najeralsc19 <najera.lsc@outlook.com>
2024-11-05 14:54:37 -0600

    peleando con el merge para el heatmap

--- dengue/pivot_dengue.py
+++ dengue/pivot_dengue.py
@@ -1,10 +1,17 @@
 import pandas as pd
 # Cargar los datos desde el archivo Excel
 dengue = pd.read_excel("E://files_pandas//dengue//Dengue_Hgo.xlsx")
-dengue.rename(columns={'CVE_MPO_RES': 'CVE_MUN'}, inplace=True)
-# Crear la tabla dinámica
-pivot = dengue.pivot_table(values='CURP', index='CVE_MUN', aggfunc='count')
+
+dengue.rename(columns={'CVE_LOC_RES': 'CVE_LOC', 'CVE_MPO_RES' : 'CVE_MUN'}, inplace=True)
+print(dengue.columns)
+ #Crear la tabla dinámica
+pivot = dengue.pivot_table(values='CURP', index=['CVE_MUN', 'CVE_LOC'], aggfunc='count')
 pivot = pivot.reset_index()
+pivot = pivot.set_index('CVE_LOC')
+
+pivot.to_excel("E://files_pandas//varios//pivote.xlsx")
+
+#print(pivot.head())
 
 
 
@@ -14,18 +21,12 @@ loc = pd.read_csv("E://files_pandas//varios//AGEEML_202410311056515.csv")
 loc = loc[loc['CVE_ENT'] == 13]
 
 
-#print(loc.columns)
-
-
-#----------------------MERGE----------------------------------------------
-
-data = pd.merge(
+merge = pd.merge(
     pivot,
-    loc[['CVE_MUN','LAT_DECIMAL','LON_DECIMAL']],
-    on=['CVE_MUN'],
+    loc[['CVE_LOC', 'CVE_MUN','LAT_DECIMAL','LON_DECIMAL']],
+    on=[['CVE_LOC', 'CVE_MUN']],
     how='left',
     suffixes=('','_loc')
-)   
+)
 
-
-data.to_excel("E://files_pandas//varios//nvo.xlsx")
+print(merge.head())
